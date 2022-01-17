47bd9e0a6e44c58d67fa36074a443226d9236f02
igorxciv <icheliadinski@gmail.com>
2022-01-17 18:17:02 -0500

    feat: add top section tabs

---
+++ apps/website/src/pages/tabs/Tabs.style.ts
@@ -0,0 +1,36 @@
+import { pageContainer } from 'design/generic/layout';
+import { rem } from 'polished';
+import styled from 'styled-components';
+import { ButtonLink } from 'ui-kit/atoms/button-link';
+import { Heading } from 'ui-kit/atoms/heading';
+import WavesIcon from 'ui-kit/icons/main-waves.svg';
+
+const TabsStyled = styled.main`
+	padding-top: ${rem(200)};
+`;
+
+const TopSection = styled.section`
+	${pageContainer}
+	display: flex;
+	flex-direction: column;
+	gap: ${rem(20)};
+	background-image: url(${WavesIcon});
+	background-repeat: no-repeat;
+	background-size: cover;
+	background-position-x: -700px;
+`;
+
+const TitleStyled = styled(Heading)`
+	font-size: ${rem(75)};
+`;
+
+const DescriptionStyled = styled.div`
+	text-align: center;
+	line-height: 1.6;
+	font-size: ${rem(22)};
+	margin-top: ${rem(30)};
+`;
+
+const SelectTabs = styled(ButtonLink)``;
+
+export { TabsStyled, TitleStyled, DescriptionStyled, SelectTabs, TopSection };
--- apps/website/src/pages/tabs/Tabs.tsx
+++ apps/website/src/pages/tabs/Tabs.tsx
@@ -1,10 +1,22 @@
+import { useStaticInfo } from 'core/static/static-context';
 import { FC } from 'react';
+import { ResponsiveImage } from 'ui-kit/atoms/responsive-image';
+import { DescriptionStyled, SelectTabs, TabsStyled, TitleStyled, TopSection } from './Tabs.style';
 
 const Tabs: FC = () => {
+	const { staticHost } = useStaticInfo();
+
 	return (
-		<main>
-			<h1>Tabs</h1>
-		</main>
+		<TabsStyled>
+			<TopSection>
+				<TitleStyled>Табы от Вани!</TitleStyled>
+				<DescriptionStyled>В каталоге имеется более 40 табов различных композиций от самых популярных исполнителей</DescriptionStyled>
+				<SelectTabs to="#tabs" size="large">
+					Выбрать табы
+				</SelectTabs>
+				<ResponsiveImage name="vanya-guitar2" staticHost={staticHost} breakpoints={{ s: 500 }} ext="png" ratio="500/291" />
+			</TopSection>
+		</TabsStyled>
 	);
 };
 
