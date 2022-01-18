1dff667687a443861aa69b2271f924dfa617b5ea
igorxciv <icheliadinski@gmail.com>
2022-01-17 22:25:59 -0500

    feat: remove forwardref

--- apps/website/src/common/header/mobile-menu/MobileMenu.tsx
+++ apps/website/src/common/header/mobile-menu/MobileMenu.tsx
@@ -1,25 +1,19 @@
-import { FC, ForwardedRef, forwardRef, HTMLAttributes, PropsWithRef } from 'react';
+import { FC, HTMLAttributes, PropsWithRef } from 'react';
 import { NavigationLink, NavigationList, NavigationStyled } from './MobileMenu.style';
 
 type Props = HTMLAttributes<HTMLElement>;
 
-const MobileMenu: FC<PropsWithRef<Props>> = forwardRef((props: Props, ref: ForwardedRef<HTMLElement>) => {
+const MobileMenu: FC<PropsWithRef<Props>> = (props: Props) => {
 	return (
-		<NavigationStyled ref={ref} {...props}>
+		<NavigationStyled {...props}>
 			<NavigationList>
-				<li>
-					<NavigationLink to="/courses">Курсы</NavigationLink>
-				</li>
-				<li>
-					<NavigationLink to="/about">Обо мне</NavigationLink>
-				</li>
 				<li>
 					<NavigationLink to="/tabs">Табы</NavigationLink>
 				</li>
 			</NavigationList>
 		</NavigationStyled>
 	);
-});
+};
 
 MobileMenu.displayName = 'MobileMenu';
 
